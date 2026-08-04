ba4534b6653382732b6ad894c8bcfe347f846f78
aakashborse <aakashvborse11@gmail.com>
2026-08-04 23:23:32 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,39 @@
+
+Name = "Borse Akash"
+Age = 21
+City= "Dhule"
+DOB = "09-04-2005"
+College = "S.S.V.P.S"
+Blood = "A+"
+
+print("-----MY ID CARD-----")
+print("Name : Borse Akash")
+print("age : 21")
+print("City : dhule")
+print("DOB : 09-04-2005")
+print("College : S.S.V.P.S")
+print("Blood_Group: A+")
+
+
+a = 25
+b = 25.5
+c = "Twenty Five"
+d= True
+e = 2+3j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d ))
+print(type(e))
+
+
+v1 = 100
+v2 = 200
+v3 = 100
+
+print(id("v1 = 100"))
+print(id("v2 = 200"))
+print(id("v3 = 300"))
+
+
+
