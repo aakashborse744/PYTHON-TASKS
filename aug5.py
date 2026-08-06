a9886fac3203eaced28ce3c2fd9e2941a3bc8c95
aakashborse <aakashvborse11@gmail.com>
2026-08-06 13:22:47 +0530

    Add files via upload

---
+++ aug5.py
@@ -0,0 +1,41 @@
+# f = 10.0
+# print(type(f))
+
+# number1 = input("true")
+# print(number1)
+# print(type(number1))
+
+
+# 1> ID CARD > ACCEPT DATA FOR ID CARD FROM CONSOLE.
+
+Name = "Borse Akash"
+Age = 21
+City= "Dhule"
+DOB = "09-04-2005"
+College = "S.S.V.P.S"
+bg = "A+"
+
+print("-----MY ID CARD-----")
+name = input("Enter your name : ")
+age =  input("Enter your age :")
+city = input("Enter your city :")
+DOB =  input("Enter you DOB :")
+college_name = input("Enter your college_name : ")
+bg = input("Enter your bg :")
+
+
+# 2> MEMORY DETECTIVE > ACCEPT DATA FOR ID CARD FROM CONSOL.
+
+v1 = 100
+v2 = 200
+v3 = 300
+
+v1 = input(id(v1))
+v2 = input(id(v2))
+v3 = input(id(v3))
+
+
+
+
+
+
